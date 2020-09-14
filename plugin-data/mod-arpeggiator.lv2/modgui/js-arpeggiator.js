125d7455ad635e68f1d1f1df3a21bc069bcb7acb
Luis Fagundes <lhfagundes@gmail.com>
2020-09-14 07:53:42 -0300

    Return the LCD of BPM in Arpeggiator for original size, and remove the "BPM" text using javascript

---
+++ plugin-data/mod-arpeggiator.lv2/modgui/js-arpeggiator.js
@@ -0,0 +1,12 @@
+function(event) {
+    // This is a workaround to remove "Bpm" string from value.
+    var bpm;
+    if (event.type == "start") {
+        bpm = event.ports.filter(function(port) { return port.symbol == "Bpm" })[0].value;
+    } else if (event.symbol == "Bpm") {
+        bpm = event.value;
+    } else {
+        return;
+    }
+    event.icon.find('[mod-role=input-control-value][mod-port-symbol=Bpm]').text(bpm.toString());
+}
